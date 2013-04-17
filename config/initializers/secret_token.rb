419e0fc7742225dd6337bbce3dd167835657bd76
limsunyoung <lsy@snuev.com>
2013-04-17 19:59:20 +0900

    First.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lsy::Application.config.secret_token = '313a25c939d92fe3d10293f9aa552612297692f9115a4681197260edeb8f15eac576819579687b72f67a1f0d978eb5d18dd54fdf2805479753dcc917d3889130'
